4a70a2a144f689aefe9cb1d5030de893c34b0774
jaanli <jaan.li@jaan.li>
2024-02-20 09:29:10 -0500

    wip: inflation adjusted historical income

--- american_community_survey/models/public_use_microdata_sample/figures/income-histogram-with-sector-historical.sql
+++ american_community_survey/models/public_use_microdata_sample/figures/income-histogram-with-sector-historical.sql
@@ -2,21 +2,23 @@
 
 WITH raw_data AS (
     SELECT
-        CAST("WAGP" AS INT) AS wage,
-        CAST("ADJINC" AS INT) AS adjustment_factor,
-        CAST("INDP" AS INT) AS industry_code,
-        SUBSTRING(SPLIT_PART(filename, '/', 6), 1, 4) AS year,
+        CAST("WAGP" AS FLOAT) AS wage,
+        CAST("ADJINC" AS FLOAT) AS adjustment_factor,
+        "INDP" AS industry_code,
+        CAST("ADJUST" AS FLOAT) AS pre_2007_adjustment_factor,
+        CAST(SUBSTRING(SPLIT_PART(filename, '/', 6), 1, 4) AS INT) AS year,
         filename
     FROM read_csv_auto('~/data/american_community_survey/*/**/*pus*.csv', filename=true, union_by_name=true)
     WHERE
         "WAGP" IS NOT NULL AND 
-        "ADJINC" IS NOT NULL AND 
+        -- "ADJINC" IS NOT NULL AND 
         "INDP" IS NOT NULL
 )
 SELECT
     wage,
     adjustment_factor,
+    pre_2007_adjustment_factor,
     industry_code,
     year
 FROM raw_data
-ORDER BY year, wage, adjustment_factor, industry_code
+ORDER BY year, wage, industry_code, adjustment_factor, pre_2007_adjustment_factor
