74d8af74be16829b45ddaa7f78b6d4bdb52cb983
jaanli <jaan.li@jaan.li>
2024-02-21 18:42:43 -0500

    fix: ordering of common table expressions to speed up query

--- american_community_survey/macros/generate_industry_mapping_2003_onwards.sql
+++ american_community_survey/macros/generate_industry_mapping_2003_onwards.sql
@@ -1,8 +1,8 @@
-{% macro generate_industry_mapping_2003_onwards_sql(input_field, output_alias) %}
+{% macro generate_industry_mapping_2003_onwards_sql(input_field) %}
 CASE {{ input_field }}
     {% set industry_mappings = get_industry_mappings_2003_onwards() %}
     {% for code, description in industry_mappings.items() %}
     WHEN '{{ code }}' THEN '{{ description }}'
     {% endfor %}
-END::ENUM ({% for description in industry_mappings.values() | unique %}'{{ description }}'{{ "," if not loop.last }}{% endfor %}) AS {{ output_alias }}
+END::ENUM ({% for description in industry_mappings.values() | unique %}'{{ description }}'{{ "," if not loop.last }}{% endfor %})
 {% endmacro %}
--- american_community_survey/macros/generate_industry_mapping_before_2003.sql
+++ american_community_survey/macros/generate_industry_mapping_before_2003.sql
@@ -1,8 +1,8 @@
-{% macro generate_industry_mapping_before_2003_sql(input_field, output_alias) %}
+{% macro generate_industry_mapping_before_2003_sql(input_field) %}
 CASE {{ input_field }}
     {% set industry_mappings = get_industry_mappings_before_2003() %}
     {% for code, description in industry_mappings.items() %}
     WHEN '{{ code }}' THEN '{{ description }}'
     {% endfor %}
-END::ENUM ({% for description in industry_mappings.values() | unique %}'{{ description }}'{{ "," if not loop.last }}{% endfor %}) AS {{ output_alias }}
+END::ENUM ({% for description in industry_mappings.values() | unique %}'{{ description }}'{{ "," if not loop.last }}{% endfor %})
 {% endmacro %}
